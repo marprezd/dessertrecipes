14a925ad3a6e2fb671e916ccfd3c0c68a3845e89
maprezdev <perezdhoz@gmail.com>
2020-01-31 19:13:57 -0500

    Update models/recipe.py

--- models/recipe.py
+++ models/recipe.py
@@ -1,7 +1,11 @@
-recipe_list = []
+# models/recipe.py file
+from extensions import db
+
+recipe_list = []  # store recipes within a list
 
 
 def get_last_id():
+    """get the ID of our last recipe"""
     if recipe_list:
         last_recipe = recipe_list[-1]
     else:
@@ -9,23 +13,18 @@ def get_last_id():
     return last_recipe.id + 1
 
 
-class Recipe:
-    def __init__(self, name, description, num_of_servings, cook_time, directions):
-        self.id = get_last_id()
-        self.name = name
-        self.description = description
-        self.num_of_servings = num_of_servings
-        self.cook_time = cook_time
-        self.directions = directions
-        self.is_publish = False
+class Recipe(db.Model):
+    """class Recipe: mapped to the recipe table in the database and define the field"""
+    __tablename__ = 'recipe'
 
-    @property
-    def data(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'description': self.description,
-            'num_of_servings': self.num_of_servings,
-            'cook_time': self.cook_time,
-            'directions': self.directions
-        }
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200))
+    num_of_servings = db.Column(db.Integer)
+    cook_time = db.Column(db.Integer)
+    directions = db.Column(db.String(1000))
+    is_publish = db.Column(db.Boolean(), default=False)
+    created_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now(), onupdate=db.func.now())
+
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
