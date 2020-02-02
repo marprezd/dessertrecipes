69dacd5939fd4b0a7f276424eab063dc1fe2605b
maprezdev <perezdhoz@gmail.com>
2020-02-02 13:38:24 -0500

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,7 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://marpezdev:l1021257@localhost/data_recipe'  # path of the database
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Configure Flask-JWT-Extend
+    SECRET_KEY = 'super-secret-key'
+    JWT_ERROR_MESSAGE_KEY = 'message'
