4344f15248ee8748d906241ed7c7987081ff05cf
maprezdev <perezdhoz@gmail.com>
2020-02-02 13:40:34 -0500

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,5 +1,9 @@
 # extensions.py file
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 
-# instance SQLAlchemy object
+# instance of SQLAlchemy object
 db = SQLAlchemy()
+# instance of Flask JWT Extended object
+jwt = JWTManager()
+
