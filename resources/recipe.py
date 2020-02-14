8b41e5b5fab608b017a68ed8bb04e6c8148bae39
maprezdev <perezdhoz@gmail.com>
2020-02-14 16:22:49 -0500

    resources/recipe.py

--- resources/recipe.py
+++ resources/recipe.py
@@ -1,50 +1,47 @@
 from flask import request
 from flask_restful import Resource
+from flask_jwt_extended import get_jwt_identity, jwt_required, jwt_optional
 from http import HTTPStatus
 from models.recipe import Recipe
-from flask_jwt_extended import get_jwt_identity, jwt_required, jwt_optional
+from schemas.recipe import RecipeSchema
+
+recipe_schema = RecipeSchema()
+recipe_list_schema = RecipeSchema(many=True)
 
 
 class RecipeListResource(Resource):
-    """implementing the RecipeListResource class, which inherits from flask-restful.Resource."""
-
     def get(self):
-        """getting all recipes"""
+        """getting all recipes
+        :return:
+        """
         recipes = Recipe.get_all_published()
-        data = []
 
-        for recipe in recipes:
-            data.append(recipe.data())
-
-        return {'data': data}, HTTPStatus.OK
+        return recipe_list_schema.dump(recipes).data, HTTPStatus.OK
 
     @jwt_required
     def post(self):
-        """
-        gets all the recipe details from the client requests and
-        saves them in the database
-        """
+        """method to gets all the recipe details"""
         json_data = request.get_json()
         current_user = get_jwt_identity()
 
-        recipe = Recipe(
-            name=json_data['name'],
-            description=json_data['description'],
-            num_of_servings=json_data['num_of_servings'],
-            cook_time=json_data['cook_time'],
-            directions=json_data['directions'],
-            user_id=current_user
-        )
+        # verify data received
+        data, errors = recipe_schema.load(data=json_data)
 
+        if errors:
+            return {'message': 'Validation errors', 'errors': errors}, HTTPStatus.BAD_REQUEST
+
+        # If verification passes, create a Recipe object
+        recipe = Recipe(**data)
+
+        # ... then save the recipe
+        recipe.user_id = current_user
         recipe.save()
 
-        # returns the recipe record with an HTTP status code 201 CREATED
-        return recipe.data(), HTTPStatus.CREATED
+        # converted recipe to JSON with a HTTP status code 201 CREATED message
+        return recipe_schema.dump(recipe).data, HTTPStatus.CREATED
 
 
 class RecipeResource(Resource):
-    """implementing the RecipeResource class, which inherits from flask-restful.Resource."""
-
     @jwt_optional
     def get(self, recipe_id):
         """This method get a specific recipe"""
@@ -107,7 +104,6 @@ class RecipeResource(Resource):
 
 
 class RecipePublishResource(Resource):
-    """implementing put method"""
     @jwt_required
     def put(self, recipe_id):
         recipe = Recipe.get_by_id(recipe_id=recipe_id)
