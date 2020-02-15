9a042e3ed7a00591e2d996551ddb11ef551c4bb9
maprezdev <perezdhoz@gmail.com>
2020-02-15 12:00:24 -0500

    Updated resources/recipe.py file

--- resources/recipe.py
+++ resources/recipe.py
@@ -55,12 +55,18 @@ class RecipeResource(Resource):
         if recipe.is_publish is False and recipe.user_id != current_user:  # we use an access control
             return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
 
-        return recipe.data(), HTTPStatus.OK
+        # returns the recipe data in JSON format and the HTTP status code 200 OK if everything goes well.
+        return recipe_schema.dump(recipe).data, HTTPStatus.OK
 
     @jwt_required
-    def put(self, recipe_id):
+    def patch(self, recipe_id):
         """This method update the recipe details"""
         json_data = request.get_json()
+        data, errors = recipe_schema.load(data=json_data, partial=('name',))
+
+        if errors:
+            return {'message': 'Validation errors', 'errors': errors}, HTTPStatus.BAD_REQUEST
+
         recipe = Recipe.get_by_id(recipe_id=recipe_id)
 
         # check whether the recipe exists and whether the user has update privileges
@@ -73,16 +79,16 @@ class RecipeResource(Resource):
             return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
 
         # update the recipe details and save it to the database
-        recipe.name = json_data['name']
-        recipe.description = json_data['description']
-        recipe.num_of_servings = json_data['num_of_servings']
-        recipe.cook_time = json_data['cook_time']
-        recipe.directions = json_data['directions']
+        recipe.name = data.get('name') or recipe.name
+        recipe.description = data.get('description') or recipe.description
+        recipe.num_of_servings = data.get('num_of_servings') or recipe.num_of_servings
+        recipe.cook_time = data.get('cook_time') or recipe.cook_time
+        recipe.directions = data.get('directions') or recipe.directions
 
         recipe.save()
 
-        # returns the HTTP status code 200 OK if everything goes well.
-        return recipe.data(), HTTPStatus.OK
+        # returns the recipe data in JSON format and the HTTP status code 200 OK if everything goes well.
+        return recipe_schema.dump(recipe).data, HTTPStatus.OK
 
     @jwt_required
     def delete(self, recipe_id):
