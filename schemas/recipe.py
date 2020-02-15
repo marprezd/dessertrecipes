b163cd6898c6ba12dfa39f0a1c81c498b1f5bb99
maprezdev <perezdhoz@gmail.com>
2020-02-15 11:53:29 -0500

    Update schemas/recipe.py file

--- schemas/recipe.py
+++ schemas/recipe.py
@@ -23,7 +23,7 @@ class RecipeSchema(Schema):
     cook_time = fields.Integer()
     directions = fields.String(validate=[validate.Length(max=1000)])
     is_publish = fields.Boolean(dump_only=True)
-    author = fields.Nested(UserSchema, attribute='user', dump_only=True, only=['id', 'username'])
+    author = fields.Nested(UserSchema, attribute='user', dump_only=True, exclude=('email', ))
     created_at = fields.DateTime(dump_only=True)
     updated_at = fields.DateTime(dump_only=True)
 
