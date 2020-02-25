49441b4ff1ed542ba8d6776766575e96a0bf8934
maprezdev <perezdhoz@gmail.com>
2020-02-25 11:57:11 -0500

    Update schemas/recipe.py file

--- schemas/recipe.py
+++ schemas/recipe.py
@@ -1,10 +1,14 @@
 # schemas/recipe.py file
+
+# Import the necessary package and module
+from flask import url_for
 from marshmallow import Schema, fields, post_dump, validate, validates, ValidationError
 from schemas.user import UserSchema
+from schemas.pagination import PaginationSchema
 
 
 def validate_num_of_servings(number):
-    """validate the 'num_of_servings' attribute. This is a customized validation function"""
+    """This function has the logic to validate the 'num_of_servings' attribute"""
     if number < 1:
         raise ValidationError('Number of servings must be greater than 0.')
     if number > 50:
@@ -12,34 +16,41 @@ def validate_num_of_servings(number):
 
 
 class RecipeSchema(Schema):
+
     class Meta:
         ordered = True
 
-    # attributes to validate and serializing
+    # Define our Recipe Schema to Validate/Serialize/Deserialize Object
     id = fields.Integer(dump_only=True)
     name = fields.String(required=True, validate=[validate.Length(max=100)])
     description = fields.String(validate=[validate.Length(max=200)])
     num_of_servings = fields.Integer(validate=validate_num_of_servings)
     cook_time = fields.Integer()
+    ingredients = fields.String(validate=[validate.Length(max=1000)])
     directions = fields.String(validate=[validate.Length(max=1000)])
+    cover_url = fields.Method(serialize='dump_cover_url')
     is_publish = fields.Boolean(dump_only=True)
     author = fields.Nested(UserSchema, attribute='user', dump_only=True, exclude=('email', ))
     created_at = fields.DateTime(dump_only=True)
     updated_at = fields.DateTime(dump_only=True)
 
-    # validate the 'cook_time' attribute. Another customized validation method using a decorator
     @validates('cook_time')
     def validate_cook_time(self, value):
-        """the cooking time should be between 1 minute and 300 minutes"""
+        """This method has the logic to validate the 'cook_time' attribute"""
         if value < 1:
             raise ValidationError('Cook time must be greater than 0.')
         if value > 300:
             raise ValidationError('Cook time must not be greater than 300.')
 
-    # finally, the processing can be done when the recipe is serialized
-    @post_dump(pass_many=True)
-    def wrap(self, data, many, **kwargs):
-        """this method return only one recipe or many recipes"""
-        if many:
-            return {'data': data}
-        return data
+    def dump_cover_url(self, recipe):
+        """This method has got the logic to verify the cover image of the recipe."""
+        if recipe.cover_image:
+            return url_for('static', filename='images/recipes/{}'.format(recipe.cover_image), _external=True)
+        else:  # set default cover image
+            return url_for('static', filename='images/assets/default-recipe-cover.jpg', _external=True)
+
+
+class RecipePaginationSchema(PaginationSchema):
+
+    # Getting the source data of the 'items' to be attributed in the paging objects.
+    data = fields.Nested(RecipeSchema, attribute='items', many=True)
